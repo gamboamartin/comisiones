a3178714a5b9d40c1202643d9b51269b4106de1b
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-12-14 15:30:24 -0600

    v3.2.0 Se modifica salida de registros de entrada

---
+++ templates/inputs/com_comision/alta.php
@@ -0,0 +1,9 @@
+<?php /** @var  \gamboamartin\facturacion\controllers\controlador_fc_factura $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->com_agente_id; ?>
+<?php echo $controlador->inputs->fc_factura_id; ?>
+<?php echo $controlador->inputs->comi_conf_comision_id; ?>
+<?php echo $controlador->inputs->monto_pago; ?>
+<?php echo $controlador->inputs->fecha_pago; ?>
+
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
