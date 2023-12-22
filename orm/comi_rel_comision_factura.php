2d4a1d1ceb5fda38eacc36ad1139e5c583cdbb19
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-12-22 10:45:17 -0600

    v3.21.0 Se ajusta modelo de relacion

--- orm/comi_rel_comision_factura.php
+++ orm/comi_rel_comision_factura.php
@@ -2,6 +2,9 @@
 namespace gamboamartin\comisiones\models;
 
 use base\orm\_modelo_parent_sin_codigo;
+use gamboamartin\errores\errores;
+use gamboamartin\facturacion\models\fc_factura;
+use stdClass;
 use PDO;
 
 
@@ -18,5 +21,40 @@ class comi_rel_comision_factura extends _modelo_parent_sin_codigo {
             columnas: $columnas,no_duplicados: $no_duplicados);
 
         $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $r_comision = (new comi_comision(link: $this->link))->registro($this->registro['comi_comision_id']);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar com tipo comision', data: $r_comision);
+        }
+
+        $r_factura = (new fc_factura(link: $this->link))->registro(
+            $this->registro['fc_factura_id']);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar factura', data: $r_factura);
+        }
+
+        if(!isset($this->registro['codigo'])){
+            $this->registro['codigo'] = $r_comision['comi_comision_descripcion']." - ".
+                $r_factura['fc_factura_descripcion']." - ".rand();
+        }
+
+        if(!isset($this->registro['descripcion'])){
+            $this->registro['descripcion'] = $r_comision['comi_comision_descripcion']." - ".rand();
+        }
+
+        if(!isset($this->registro['descripcion_select'])){
+            $this->registro['descripcion_select'] = $r_comision['comi_comision_descripcion']." - ".
+                $r_factura['fc_factura_descripcion'];
+        }
+
+        $r_alta_bd = parent::alta_bd($keys_integra_ds);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar solicitud etapa', data: $r_alta_bd);
+        }
+
+        return $r_alta_bd;
     }
 }
