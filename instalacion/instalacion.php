ea3e8497833581824274a00e5127f74f29b044d4
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-02-23 14:32:26 -0600

    v5.6.0 Limpieza de entidad y campo no relacionado

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -109,30 +109,6 @@ class instalacion
         return $out;
     }
 
-    private function _add_comi_rel_comision_factura(PDO $link): array|stdClass
-    {
-        $out = new stdClass();
-        $create = (NEW _instalacion($link))->create_table_new(table:'comi_rel_comision_factura');
-        if(errores::$error){
-            return (new errores())->error(mensaje: 'Error al crear comi_rel_comision_factura', data: $create);
-        }
-
-        $out->create = $create;
-
-        $foraneas = array();
-        $foraneas['fc_factura_id'] = new stdClass();
-        $foraneas['comi_comision_id'] = new stdClass();
-
-        $foraneas_r = (new _instalacion(link:$link))->foraneas(foraneas: $foraneas,table:  'comi_rel_comision_factura');
-
-        if(errores::$error){
-            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $foraneas_r);
-        }
-        $out->foraneas_r = $foraneas_r;
-
-        return $out;
-    }
-
     private function _add_comi_tipo_comision(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -176,21 +152,6 @@ class instalacion
 
     }
 
-    private function comi_rel_comision_factura(PDO $link): array|stdClass
-    {
-        $out = new stdClass();
-
-        $create = $this->_add_comi_rel_comision_factura(link: $link);
-        if(errores::$error){
-            return (new errores())->error(mensaje: 'Error al agregar tabla', data:  $create);
-        }
-
-        $out->campos = $create;
-
-        return $out;
-
-    }
-
     private function comi_tipo_comision(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -216,12 +177,6 @@ class instalacion
             return (new errores())->error(mensaje: 'Error integrar comi_tipo_comision', data:  $comi_tipo_comision);
         }
         $out->comi_tipo_comision = $comi_tipo_comision;
-
-        $comi_rel_comision_factura = $this->comi_rel_comision_factura(link: $link);
-        if(errores::$error){
-            return (new errores())->error(mensaje: 'Error integrar comi_rel_comision_factura', data:  $comi_rel_comision_factura);
-        }
-        $out->comi_rel_comision_factura = $comi_rel_comision_factura;
 
         $comi_conf_comision = $this->comi_conf_comision(link: $link);
         if(errores::$error){
