1029aa4d5a07a4362d2494e9e46172f2eb4ce7a7
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-03-11 15:47:03 -0600

    v7.2.0 Se integra catalogo faltante cs

---
+++ controllers/controlador_cat_sat_periodo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\comisiones\controllers;
+
+use gamboamartin\cat_sat\models\cat_sat_periodo;
+
+class controlador_cat_sat_periodo extends cat_sat_periodo {}
