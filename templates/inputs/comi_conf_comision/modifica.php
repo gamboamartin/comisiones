acd6e8d898a134e4e962032e31c4cf9da717240e
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-03-14 10:52:59 -0600

    v9.2.0 Se ajsute de elementos mostrados y comentario

--- templates/inputs/comi_conf_comision/modifica.php
+++ templates/inputs/comi_conf_comision/modifica.php
@@ -3,6 +3,7 @@
 <?php echo $controlador->inputs->fecha_inicial; ?>
 <?php echo $controlador->inputs->fecha_final; ?>
 <?php echo $controlador->inputs->monto_pago; ?>
+<?php echo $controlador->inputs->cat_sat_periodo_id; ?>
 <?php echo $controlador->inputs->com_tipo_agente_id; ?>
 <?php echo $controlador->inputs->comi_tipo_comision_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
