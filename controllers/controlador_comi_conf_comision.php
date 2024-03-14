5dc5e3d5930535cd1c88fa373ffdce37fa00e1ec
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-03-14 10:28:52 -0600

    v9.1.0 Se integra selector ajustado

--- controllers/controlador_comi_conf_comision.php
+++ controllers/controlador_comi_conf_comision.php
@@ -55,8 +55,14 @@ class controlador_comi_conf_comision extends _ctl_parent_sin_codigo {
             return $this->retorno_error(mensaje: 'Error al inicializar alta', data: $r_alta, header: $header, ws: $ws);
         }
 
+        $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  array(), key: 'cat_sat_periodo_id',
+            keys_selects: array(), id_selected: -1, label: 'Periodo');
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects, header: $header,ws:  $ws);
+        }
+
         $keys_selects = $this->key_select(cols:6, con_registros: true,filtro:  array(), key: 'com_tipo_agente_id',
-            keys_selects: array(), id_selected: -1, label: 'Tipo Agente');
+            keys_selects: $keys_selects, id_selected: -1, label: 'Tipo Agente');
         if(errores::$error){
             return $this->retorno_error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects, header: $header,ws:  $ws);
         }
