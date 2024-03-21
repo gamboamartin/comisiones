377d0058df746e1275abb9c1277f94806dcaf767
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-03-21 16:22:16 -0600

    v9.10.0 Se genera funcion de obtencion de peri

--- templates/inputs/comi_comision/modifica.php
+++ templates/inputs/comi_comision/modifica.php
@@ -3,6 +3,7 @@
 <?php echo $controlador->inputs->fc_factura_id; ?>
 <?php echo $controlador->inputs->com_agente_id; ?>
 <?php echo $controlador->inputs->comi_conf_comision_id; ?>
+<?php echo $controlador->inputs->cat_sat_periodo_id; ?>
 <?php echo $controlador->inputs->monto_pago; ?>
 <?php echo $controlador->inputs->fecha_pago; ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
